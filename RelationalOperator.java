b28d1a6b169b394284912ecdb82ec4584829c3d9
ashith-prog <ashithakshita@gmail.com>
2026-03-26 04:44:45 +0530

    Updated control structures

--- RelationalOperator.java
+++
@@ -1,8 +0,0 @@
-public class RelationalOperator {
-    public static void main(String[] args) {
-        int a=10;
-        int b=6;
-        if(a<b){
-        System.out.println("A is ");
-    }
-}
